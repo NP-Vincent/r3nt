7f956fa6d740f248f5434b8bfdf6bb8341633577
NP-Vincent <n.p.vincent@icloud.com>
2025-08-21 15:14:23 +0400

    Delete js/index.js

--- js/index.js
+++
@@ -1,7 +0,0 @@
-// /js/index.js
-import { ready, maybeShowReadOnlyBanner } from "./shared.js";
-
-window.addEventListener("DOMContentLoaded", async () => {
-  await maybeShowReadOnlyBanner();
-  await ready();
-});
