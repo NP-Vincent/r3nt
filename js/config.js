4fd53f77a2ebe59333b2c9ac8be5de9f35f0d0bb
NP-Vincent <n.p.vincent@icloud.com>
2025-09-07 22:57:43 +0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,8 @@ export const R3NT_ADDRESS = "0x18Af5B8fFA27B8300494Aa1a8c4F6AE4ee087029";
 export const FACTORY_ADDRESS = "0x7c67FDcebc883C1BACd03Ee7483e8E6300F4Df51";
 export const USDC_ADDRESS = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831";
 export const PLATFORM_ADDRESS = "0x43F3f03d89290358034993aE3B3938D056D76De2";
+export const REGISTRY_ADDRESS = "0x0";
+
 
 export const APP_NAME = "r3nt";
 export const APP_DOMAIN = "r3nt.sqmu.net"; // origin used in EIP-712 domain if needed
