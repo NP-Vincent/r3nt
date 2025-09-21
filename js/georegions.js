bf79ec0d7e085fbfaf7da93778ce78967b88d30e
NP-Vincent <n.p.vincent@icloud.com>
2025-09-21 08:33:12 +0400

    Update georegions.js

--- js/georegions.js
+++ js/georegions.js
@@ -1,6 +1,4 @@
 const GEOHASH_REGION_PREFIXES = Object.freeze({
-  thrr: 'Dubai',
-  thr: 'Dubai',
   '9q8': 'San Francisco Bay Area',
   '9q5c': 'Los Angeles',
   dr5: 'New York City',
@@ -8,14 +6,38 @@ const GEOHASH_REGION_PREFIXES = Object.freeze({
   gcpv: 'London',
   u33d: 'Berlin',
   u09t: 'Paris',
-  xn77: 'Tokyo',
-  wecn: 'Hong Kong',
-  w21z: 'Singapore',
   '9v6k': 'Austin',
   dhwf: 'Miami',
   c2b2: 'Vancouver',
-  r3gx: 'Sydney',
   '6gyf': 'São Paulo',
+  
+  // UAE
+  thrr: 'Dubai',
+  thrs: 'Ras Al Khaimah',
+  thes: 'Abu Dhabi',
+  ther: 'Al Ain - Abu Dhabi',
+  thrt: 'Hatta - Dubai',
+  thqq: 'Sharjah',
+  thqs: 'Kalba - Sharjah',
+  thqp: 'Fujairah',
+  thqr: 'Ajman',
+  thqj: 'Umm Al Quwain',
+
+  // Asia
+  w4kp: 'Phuket',
+  w4v0: 'Bangkok',
+  w21z: 'Singapore', // already in your list
+  w2fb: 'Kuala Lumpur',
+  tc5:  'Sri Lanka',
+  wd8:  'Maldives',
+  qzzh: 'Bali',
+  xn77: 'Tokyo',
+  wecn: 'Hong Kong',
+  r3gx: 'Sydney',
+
+  // Indian Ocean
+  mbz:  'Seychelles'
+
 });
 
 export function lookupRegionForGeohash(geohash) {
