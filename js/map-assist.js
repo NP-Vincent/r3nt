8551ed713c66992ccca554edda402296924d0c73
NP-Vincent <n.p.vincent@icloud.com>
2025-09-27 01:31:03 +0400

    Update map-assist.js

--- js/map-assist.js
+++ js/map-assist.js
@@ -1,2 +1,11 @@
 // map-assist.js
 // Helper utilities to open app the Mini App.
+// inside renderListings(...), after computing `preciseCoords = `${record.lat.toFixed(6)},${record.lon.toFixed(6)}``
+geoLine.append(
+  el('a', {
+    href: `https://www.google.com/maps/search/?api=1&query=${preciseCoords}`,
+    target: '_blank',
+    rel: 'noopener',
+    class: 'geo-map-link',
+  }, 'Open map'),
+);
